3563b82d0a77ab8e5cbcc293c1a588a577fd1b33
LIYAMANO-o <12277260741@qq.com>
2020-10-02 21:53:30 +0800

    work

---
+++ build/precache-manifest.5d58243e345c1eddb31ae1b430a1ee55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "095cfbd773c91d3aa888c1e8275b1a0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7546d12b22fdc31f70b2",
+    "url": "/static/js/2.0ffec528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0ffec528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "292ce25f235514646684",
+    "url": "/static/js/main.ea31f057.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
